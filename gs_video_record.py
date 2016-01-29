edaae05f8e4fd5d6e35825d35277674dc4c8dcfa
Thomas Layh <thomas.layh@air191.int.hq.aoe.lan>
2016-01-29 16:19:17 +0100

    add some face detection in diffVideo

--- gs_video_record.py
+++ gs_video_record.py
@@ -4,42 +4,46 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # todo: what codec works with osx
-#out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+# out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 diffFrame = None
 lastFrame = None
 
-while (cap.isOpened()):
-    ret, frame = cap.read()
+face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
 
+while cap.isOpened():
+    ret, frame = cap.read()
     if ret is True:
-        # fgmask = fgbg.apply(frame)
+
+        # make the image in grayscale
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # detect faces in frame and paint in diffFrame later
+        faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+
+        # blur the image
+        frame = cv2.blur(frame, (20, 20))
 
         if lastFrame is not None:
             diffFrame = cv2.subtract(frame, lastFrame)
-            print("DiffFrame Size:" + str(diffFrame.size))
-
-        # write the flipped frame
-        # out.write(diffFrame)
+            # print("DiffFrame Size:" + str(diffFrame.size))
 
         if diffFrame is not None:
-            # cv2.imshow("frame", frame)
+            # mark faces in diffFrame
+            for (x, y, w, h) in faces:
+                diffFrame = cv2.rectangle(diffFrame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                roi_color = diffFrame[y:y + h, x:x + w]
+
+            # print("Showing diffFrame")
             cv2.imshow('diffFrame', diffFrame)
 
-        k = cv2.waitKey(0)
-        if k == 27:         # wait for ESC key to exit
-            cv2.destroyAllWindows()
-        elif k == ord('s'): # wait for 's' key to save and exit
-            # Write the image
-            # cv2.imwrite('messigray.png',img)
-            cv2.destroyAllWindows()
+        lastFrame = frame
 
-    else:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release everything if job is finished
 cap.release()
-out.release()
 cv2.destroyAllWindows()
