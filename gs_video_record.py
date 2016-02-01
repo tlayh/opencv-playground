3518a45c7b41905f465b398a7be4ec52dea74d2d
Thomas Layh <develop@layh.com>
2016-02-01 09:04:35 +0100

    add motion detection stuff

--- gs_video_record.py
+++ gs_video_record.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import cv2
 
@@ -12,34 +13,40 @@ diffFrame = None
 lastFrame = None
 
 face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_eye.xml')
 
 while cap.isOpened():
     ret, frame = cap.read()
     if ret is True:
 
         # make the image in grayscale
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # detect faces in frame and paint in diffFrame later
-        faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+        faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
 
         # blur the image
-        frame = cv2.blur(frame, (20, 20))
+        # frame = cv2.blur(frame, (20, 20))
 
         if lastFrame is not None:
             diffFrame = cv2.subtract(frame, lastFrame)
             # print("DiffFrame Size:" + str(diffFrame.size))
 
         if diffFrame is not None:
-            # mark faces in diffFrameq
+            # mark faces in diffFrame
             for (x, y, w, h) in faces:
                 diffFrame = cv2.rectangle(diffFrame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                roi_color = diffFrame[y:y + h, x:x + w]
+                roi_color = frame[y:y + h, x:x + w]
+                roi_gray = gray_frame[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(diffFrame, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
             # print("Showing diffFrame")
             cv2.imshow('diffFrame', diffFrame)
 
         lastFrame = frame
+        time.sleep(.250)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
